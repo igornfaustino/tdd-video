92777497498c941dc0a792f530f51bb40a62adeb
igornfaustino <igor@nfaustino.com>
2023-10-17 22:41:04 -0300

    feat: ensure returns 5 when parked for exactly 5 hours

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -21,9 +21,16 @@ it('if a car is parked for 2:10 hours, the price should be 3 real', () => {
 	expect(result).toBe(3);
 });
 
-it('if a car is parked for more than 10 hours, the price should be 10 real', () => {
+it('if a car is parked for more than 5 hours, the price should be 10 real', () => {
 	const parkedDate = new Date('2020-10-10T12:00:00');
 	const exitDate = new Date('2020-10-10T17:01:00');
 	const result = getPrice(parkedDate, exitDate);
 	expect(result).toBe(10);
+});
+
+it('if a car is parked for exact 5 hours, the price should be 5 real', () => {
+	const parkedDate = new Date('2020-10-10T12:00:00');
+	const exitDate = new Date('2020-10-10T17:00:00');
+	const result = getPrice(parkedDate, exitDate);
+	expect(result).toBe(5);
 });
