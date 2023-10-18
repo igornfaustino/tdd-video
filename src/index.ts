219b5a72979fbaa0e876a8a81d3c1bf0a14ace9e
igornfaustino <igor@nfaustino.com>
2023-10-18 12:08:27 -0300

    refactor: extract convert ms to hours into a function

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 export const getPrice = (enterDate: Date, exitDate: Date) => {
 	const diffInMs = exitDate.getTime() - enterDate.getTime();
-	const diffInHours = diffInMs / (1000 * 60 * 60);
+	const diffInHours = convertMsToHours(diffInMs);
 	if (diffInHours > 5) return 10;
 	return Math.ceil(diffInHours);
+};
+
+const convertMsToHours = (ms: number) => {
+	return ms / (1000 * 60 * 60);
 };
